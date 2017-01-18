59923e7d804e152fd6aaa831b66a096442b40769
Fredy-Edwin Esse <eleegia@gmail.com>
2017-01-17 22:16:20 -0800

    Added tables and social widgets

--- js/app.js
+++ js/app.js
@@ -2,10 +2,42 @@ const notifications = document.getElementById("avatar");
 const showAlert = document.getElementsByClassName("hide")[0];
 const closeAlert = document.getElementById("alert-delete");
 
+const showTrafficHour = document.getElementById("traffic-h");
+const showTrafficDay = document.getElementById("traffic-d");
+const showTrafficWeek = document.getElementById("traffic-w");
+const showTrafficMonth = document.getElementById("traffic-m");
+
+const hourlyBtn = document.getElementById("hourly");
+const dailyBtn = document.getElementById("daily");
+const weeklyBtn = document.getElementById("weekly");
+const monthlyBtn = document.getElementById("monthly");
+
+const twitterNumber = document.getElementById("twitter");
+const facebookNumber = document.getElementById("facebook");
+const googleNumber = document.getElementById("google");
 
 window.onload = function() {
-	showAlert.style.display = "block";
-}
+	showAlert.style.display = "block"; //To show alert
+	showTrafficHour.style.display = "block"; //To show hourly traffic line chart on pg load
+
+    const randomNumber = Math.floor(Math.random() * 11000);	// Number for Twitter
+    const paragraphT = document.createElement("p");
+    paragraphT.className = "social-number";
+    paragraphT.textContent = randomNumber;
+    twitterNumber.appendChild(paragraphT);
+
+	const randomNumber1 = Math.floor(Math.random() * 11000); // Number for Facebook
+    const paragraphFb = document.createElement("p");
+    paragraphFb.className = "social-number";
+    paragraphFb.textContent = randomNumber1;
+    facebookNumber.appendChild(paragraphFb);
+
+    const randomNumber2 = Math.floor(Math.random() * 11); // Number for Google
+    const paragraphG = document.createElement("p");
+    paragraphG.className = "social-number";
+    paragraphG.textContent = randomNumber2;
+    googleNumber.appendChild(paragraphG);
+};
 
 //Show and hide notifications menu
 notifications.addEventListener("click", (e) => {
@@ -17,3 +49,61 @@ closeAlert.addEventListener("click", (e) => {
 	const tester = document.getElementById("alert");
 	tester.style.display = "none";
 });
+
+// Show daily traffic line chart
+hourlyBtn.addEventListener("click", (e) => {
+	showTrafficDay.style.display = "none";
+	showTrafficWeek.style.display = "none";
+	showTrafficMonth.style.display = "none";
+	showTrafficHour.style.display = "block";
+});
+
+// Show daily traffic line chart
+dailyBtn.addEventListener("click", (e) => {
+	showTrafficHour.style.display = "none";
+	showTrafficWeek.style.display = "none";
+	showTrafficMonth.style.display = "none";
+	showTrafficDay.style.display = "block";
+});
+
+// Show weekly traffic line chart
+weeklyBtn.addEventListener("click", (e) => {
+	showTrafficHour.style.display = "none";
+	showTrafficDay.style.display = "none";
+	showTrafficMonth.style.display = "none";
+	showTrafficWeek.style.display = "block";
+});
+
+// Show monthly traffic line chart
+monthlyBtn.addEventListener("click", (e) => {
+	showTrafficHour.style.display = "none";
+	showTrafficDay.style.display = "none";
+	showTrafficWeek.style.display = "none";
+	showTrafficMonth.style.display = "block";
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- js/charts.js
+++ js/charts.js
@@ -1,117 +1,168 @@
 // Line chart
-var traffic = document.getElementById("traffic").getContext("2d");
-var lineChart = new Chart(traffic, {
-	type: 'line',
-	data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
-        }
-    }
-});
+	// Hourly chart
+	var trafficHour = document.getElementById("traffic-h").getContext("2d");
+	var lineChartHour = new Chart(trafficHour, {
+		type: 'line',
+		data: {
+	        labels: ["00:00", "03:00", "06:00", "09:00", "12:00", "15:00", "18:00", "21:00"],
+	        datasets: [{
+	            label: '# of Votes',
+	            data: [10, 7, 10, 50, 65, 75, 60, 30],
+	            backgroundColor: [
+	                'rgba(140,125,237,.3)',
+	            ],
+	            borderColor: [
+	                'rgb(140,125,237)'
+	            ],
+	            borderWidth: 1,
+	            lineTension: 0,
+	            pointBackgroundColor: '#fff'
+	        }]
+	    },
+	    options: {
+	        legend: {
+	            display: false
+	        }
+	    }
+	});
+	var hideDisplayHour = document.getElementById("traffic-h");
+	hideDisplayHour.style.display = "none";
 
+	// Daily chart
+	var trafficDay = document.getElementById("traffic-d").getContext("2d");
+	var lineChartDay = new Chart(trafficDay, {
+		type: 'line',
+		data: {
+	        labels: ["S", "M", "T", "W", "T", "F", "S"],
+	        datasets: [{
+	            label: '# of Votes',
+	            data: [50, 75, 150, 100, 200, 175, 75],
+	            backgroundColor: [
+	                'rgba(140,125,237,.3)',
+	            ],
+	            borderColor: [
+	                'rgb(140,125,237)'
+	            ],
+	            borderWidth: 1,
+	            lineTension: 0,
+	            pointBackgroundColor: '#fff'
+	        }]
+	    },
+	    options: {
+	        legend: {
+	            display: false
+	        }
+	    }
+	});
+	var hideDisplayDay = document.getElementById("traffic-d");
+	hideDisplayDay.style.display = "none";
+
+	//Weekly chart
+	var trafficWeek = document.getElementById("traffic-w").getContext("2d");
+	var lineChartWeek = new Chart(trafficWeek, {
+		type: 'line',
+		data: {
+	        labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
+	        datasets: [{
+	            label: '# of Votes',
+	            data: [500, 1000, 750, 1250, 1200, 1750, 775, 1000, 1800, 1900, 2100],
+	            backgroundColor: [
+	                'rgba(140,125,237,.3)',
+	            ],
+	            borderColor: [
+	                'rgb(140,125,237)'
+	            ],
+	            borderWidth: 1,
+	            lineTension: 0,
+	            pointBackgroundColor: '#fff'
+	        }]
+	    },
+	    options: {
+	        legend: {
+	            display: false
+	        }
+	    }
+	});
+	var hideDisplayWeek = document.getElementById("traffic-w");
+	hideDisplayWeek.style.display = "none";
+
+	// Monthly chart
+	var trafficMonth = document.getElementById("traffic-m").getContext("2d");
+	var lineChartMonth = new Chart(trafficMonth, {
+		type: 'line',
+		data: {
+	        labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+	        datasets: [{
+	            label: '# of Votes',
+	            data: [5000, 10000, 7500, 12500, 12000, 17500, 7750, 10000, 18000, 19000, 21000, 15000],
+	            backgroundColor: [
+	                'rgba(140,125,237,.3)',
+	            ],
+	            borderColor: [
+	                'rgb(140,125,237)'
+	            ],
+	            borderWidth: 1,
+	            lineTension: 0,
+	            pointBackgroundColor: '#fff'
+	        }]
+	    },
+	    options: {
+	        legend: {
+	            display: false
+	        }
+	    }
+	});
+	var hideDisplayMonth = document.getElementById("traffic-m");
+	hideDisplayMonth.style.display = "none";
 
 // Bar chart
 var dailyTraffic = document.getElementById("daily-traffic").getContext("2d");
 var barChart = new Chart(dailyTraffic, {
     type: 'bar',
     data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        labels: ["S", "M", "T", "W", "T", "F", "S"],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            data: [50, 75, 150, 100, 200, 175, 75],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                '#8c7ded',
+                '#8c7ded',
+                '#8c7ded',
+                '#8c7ded',
+                '#8c7ded',
+                '#8c7ded',
+                '#8c7ded'
             ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
+            cornerRadius: 10,
         }]
     },
     options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
+        legend: {
+            display: false
         }
     }
 });
 
-// Pie (mmmmmmmm...) chart
+// Doughnut (mmmmmmmm...) chart
 var mobileUsers = document.getElementById("mobile-users").getContext("2d");
-var pieChart = new Chart(mobileUsers, {
-	type: 'pie',
+var doughnutChart = new Chart(mobileUsers, {
+	type: 'doughnut',
 	data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        labels: ["Desktop", "Tablets", "Phones"],
         datasets: [{
             label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            data: [65, 18, 17],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                '#8c7ded',
+                '#8ad199',
+                '#8abbd1'
             ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
+            borderWidth: 0
         }]
     },
     options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
+        legend: {
+            display: true,
+            position: 'right'
         }
     }
 });
